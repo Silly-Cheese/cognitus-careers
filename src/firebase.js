7c0491fd0db5666dc7ed82f6303f401d77962e8e
Executive_Eagle <christophershelley257@gmail.com>
2026-06-21 12:27:19 -0500

    Add Firebase client setup

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getFunctions } from 'firebase/functions';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyACKZhEsOaiYkAU_mwLJNgU7RF58TAqaPE',
+  authDomain: 'cognitus-car.firebaseapp.com',
+  projectId: 'cognitus-car',
+  storageBucket: 'cognitus-car.firebasestorage.app',
+  messagingSenderId: '904900902243',
+  appId: '1:904900902243:web:2b1bd2cdc0362b0a86a756'
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const functions = getFunctions(app);
